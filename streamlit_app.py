774230755e44d76999a22c905e30a5cfdbcfe7b7
Navneet Sindhu <128820939+NavneetSindhu@users.noreply.github.com>
2025-08-05 02:19:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -145,13 +145,13 @@ if st.session_state.issue_description_done and not st.session_state.followup_don
 if st.session_state.followup_done:
     contact = find_contact_email(st.session_state.location, st.session_state.issue_type)
     st.subheader("📩 Contact Details")
-      with st.status("Searching for Contact"):
-        st.write("Searching on google")
-        time.sleep(2)
-        st.write("Found URL.")
-        time.sleep(1)
-        st.write("Retrieving Address")
-        time.sleep(1)
+with st.status("Searching for Contact"):
+    st.write("Searching on google")
+    time.sleep(2)
+    st.write("Found URL.")
+    time.sleep(1)
+    st.write("Retrieving Address")
+    time.sleep(1)     
 
 st.button("Rerun")
     st.code(contact)
